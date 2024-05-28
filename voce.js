71ebe4f334edae8d5ef5c77111549e811c94e0a4
Baranidharan P <140156129+Barani-p@users.noreply.github.com>
2024-05-28 22:01:49 +0530

    Add files via upload

---
+++ voce.js
@@ -0,0 +1,13 @@
+const reg = window.speechRecognition || window.webkitspeechRecognition;
+const speech=new reg();
+console.log(speech)
+// function start1(){
+//     speechSynthesis.start()
+// Speech.o nresult=function(e){
+//     document.getElementById('output').Intertext="your said: "te.results[e.resultsIndex] [0].transcript;
+// }
+// Speech.onend=function(){
+//     document.getElementById("res").ATTRIBUTE_NODE
+//     Innertext="if you want to talk again click the mic icon";
+// }
+// }
